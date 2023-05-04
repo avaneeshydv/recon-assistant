9020782d772187e7ebc694ec09203d3929fc9c2e
kanak.goel <kanak.goel@payu.in>
2023-05-05 01:11:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState } from "react";
 import './App.css';
 
 import Start from "./Start";
@@ -8,12 +8,26 @@ import Page2 from "./Page2";
 
 
 function App() {
+    const [rows, setRows] = useState([
+        {
+            id: 1,
+            title: 'This is first list'
+        },
+        {
+            id: 2,
+            title: 'This is second list'
+        },
+        {
+            id: 3,
+            title: 'This is third list'
+        },
+    ]);
   return (
     <div className="App">
 
 
       <Start />
-        <Page2 />
+        <Page2 rows={rows}/>
     </div>
   );
 }
