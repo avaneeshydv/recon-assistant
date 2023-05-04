360e3ee4351061da1e163ec5081be73328cab1b0
kanak.goel <kanak.goel@payu.in>
2023-05-05 00:32:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Start from "./Start";
 function App() {
   return (
     <div className="App">
-      <h1>Recon Assitant</h1>
+
 
       <Start />
 
--- src/Start.jsx
+++ src/Start.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import "./Start.css";
-import AttachFileIcon from '@mui/icons-material/AttachFile';
+import Card from "./Card";
+import "./Start.css"
+import Button from "./Button";
+
+
+
 
 
 function Start() {
   return (
     <div className="start">
-      <h1>Identify File</h1>
-      <div className="split left">
-        <div className="centered">
-          <AttachFileIcon />
-          <h2>Jane Flex</h2>
-          <p>Some text.</p>
-        </div>
+      <h1 className=''>Generative Recon</h1>
+      <div className="second_start">
+        <h3>Source</h3>
+        <h3>Destination</h3>
+      </div>
+      <div className='third_start'>
+        <Card className="card_one" />
+        <Card className="card_two" />
+      </div>
+      <div className='fourth_start'>
+        <Button />
+        <Button />
+
       </div>
 
-      <div className="split right">
-        <div className="centered">
-          <AttachFileIcon />
-          <h2>John Doe</h2>
-          <p>Some text here too.</p>
-        </div>
-      </div>
+
+
     </div>
 
   )
