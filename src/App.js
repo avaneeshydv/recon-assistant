9e061dd1ec16d0af5e14012177c0cb577d16765d
Tanishq Jagwani <tanishq.jagwani@GLFVFFF5E7Q05D.local>
2023-05-04 20:29:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import JsonDataDisplay from "./JsonDataDisplay";
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Recon Assitant</h1>
+     <JsonDataDisplay />
+
+    </div>
+  );
+}
+
+export default App;
